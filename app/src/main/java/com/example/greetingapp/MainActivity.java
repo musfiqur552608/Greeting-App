e6b9b72822f935a9c21f28e4b5edef70089c55db
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-07-12 00:24:53 +0600

    add backend

--- app/src/main/java/com/example/greetingapp/MainActivity.java
+++ app/src/main/java/com/example/greetingapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.greetingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText name;
+    private Button send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.greetingid);
+        send = findViewById(R.id.sendbtn);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameText = name.getText().toString();
+                Toast.makeText(MainActivity.this, "Welcome "+nameText, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
